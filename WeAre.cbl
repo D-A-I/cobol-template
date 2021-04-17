8b3dfc3b512b304e482ae824df1d9cbf08a2ca03
D--A--I <kanegadai@kanegadainoMacBook-Pro.local>
2021-04-17 23:59:38 +0900

    first commit

---
+++ WeAre.cbl
@@ -0,0 +1,10 @@
+IDENTIFICATION                   DIVISION.
+PROGRAM-ID.                      WE-ARE.
+ENVIRONMENT                      DIVISION.
+DATA                             DIVISION.
+PROCEDURE                        DIVISION.
+MAIN.
+    DISPLAY "WE" UPON CONSOLE.
+    DISPLAY " ARE" UPON CONSOLE.
+    DISPLAY "  TG!!!" UPON CONSOLE.
+    STOP RUN.
